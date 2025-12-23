63a1bab7cce5fd38b4928a9014e7ca433ddc57d8
Dhruvika Purohit <purohitdhruvika87@gmail.com>
2025-12-23 20:25:45 +0530

    Add files via upload

---
+++ basics/swapping.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c;
+	printf("enter the value of a = ");
+	scanf("%d",&a);
+	printf("enter the value of b = ");
+	scanf("%d",&b);
+	
+	c=a;
+	a=b;
+	b=c;
+	
+	printf("value of a is %d\n",a);
+	printf("value of b is %d",b);
+}
---
+++ basics/swapping_without_third_variable.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b;
+	printf("enter the value of a = ");
+	scanf("%d",&a);
+	printf("enter the value of b = ");
+	scanf("%d",&b);
+	
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	
+	printf("value of a is %d\n",a);
+	printf("value of b is %d",b);
+}
