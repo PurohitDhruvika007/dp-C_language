9db7149a2363c126344cb5256007f640cdee0d43
Dhruvika Purohit <purohitdhruvika87@gmail.com>
2025-12-24 21:23:57 +0530

    Add files via upload

---
+++ conditional_statement/minimum_3_nested.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c;
+	printf("enter the value of a,b,c = ");
+	scanf("%d%d%d",&a,&b,&c);
+	
+	if(a<b)
+	{
+		if(a<c)
+		{
+			printf("a is minimum");
+		}
+		else
+		{
+			printf("c is minimum");
+		}
+	}
+	else
+	{
+		if(b<c)
+		{
+			printf("b is minimum");
+		}
+		else
+		{
+			printf("c is minimum");
+		}
+	}
+}
