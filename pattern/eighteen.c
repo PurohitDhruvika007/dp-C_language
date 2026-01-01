971c0da7353ddf380c713e65fdfee0e487659873
Dhruvika Purohit <purohitdhruvika87@gmail.com>
2026-01-01 18:02:03 +0530

    Add files via upload

---
+++ pattern/eighteen.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+//0
+//0 1
+//0 1 0
+//0 1 0 1
+//0 1 0 1 0
+	
+	int i,j;
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			if(j%2==0)
+			{
+				printf("1 ");
+			}
+			else
+			{
+				printf("0 ");
+			}
+			
+		}
+		printf("\n");
+	}
+}
