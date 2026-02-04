5b48255675925c2294458184e950d323fdd18daf
Dhruvika Purohit <purohitdhruvika87@gmail.com>
2026-02-04 18:42:18 +0530

    Update one.c

--- pr_8/one.c
+++ pr_8/one.c
@@ -4,9 +4,12 @@
 int main()
 {
 	char str[]="hello world";
-	
-	
-	printf("%s\n",str);
-	printf("length of str = %lu",strlen(str));
+	printf("string is %s\n",str);
+	int i=0;
+	while(str[i]!='\0')
+	{
+		i++;
+	}
+	printf("length of string is %d",i);
 	return 0;
 }
